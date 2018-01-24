6e32fabc2ae08db711f638999962fc0b23982463
Ks-Mosqueda <ks.mosqueda@gmail.com>
2018-01-24 10:24:43 -0600

    cambiando tipografia

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
-
+$(document).ready(function () {
+  setTimeout(function () {
+    var mostrar= document.getElementById("fondoNegro");
+    var mostrarInicio = document.getElementById("inicio");
+    mostrar.classList.remove("show");
+    mostrar.classList.add("hide");
+    mostrarInicio.classList.remove("hide");
+    mostrarInicio.classList.add("show");
+  },3000);
+});
 
 
 
